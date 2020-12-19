c969bdc4ccc23a17fe5fb565546efe3a4043e663
cy <micheal.caoy@gmail.com>
2020-12-20 03:43:07 +0800

    -- 删除舵机2

--- steering/control_steering.py
+++ steering/control_steering.py
@@ -13,16 +13,17 @@ GPIO.setwarnings(False)
 
 def set_servo_angle(servo, angle):
     """控制舵机"""
+    GPIO.setup(servo, GPIO.OUT)
     pwm = GPIO.PWM(servo, 50)
     pwm.start(8)
     duty_cycle = angle / 18. + 3.
     pwm.ChangeDutyCycle(duty_cycle)
     sleep(0.3)
     pwm.stop()
+    GPIO.cleanup()
 
 
 if __name__ == '__main__':
     servo = int(sys.argv[1])
-    GPIO.setup(servo, GPIO.OUT)
     set_servo_angle(servo, int(sys.argv[2]))
-    GPIO.cleanup()
+
