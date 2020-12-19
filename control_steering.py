9d6710214d6757db6a7d8a460a663bcd5d668f58
cy <micheal.caoy@gmail.com>
2020-12-20 02:27:39 +0800

    -- 增加 舵机

---
+++ control_steering.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @create_time : 2020/12/20 2:17 上午
+# @Author : CY
+# @File : control_steering.py
+
+from time import sleep
+import RPi.GPIO as GPIO
+import sys
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+
+def set_servo_angle(servo, angle):
+    """控制舵机"""
+    pwm = GPIO.PWM(servo, 50)
+    pwm.start(8)
+    duty_cycle = angle / 18. + 3.
+    pwm.ChangeDutyCycle(duty_cycle)
+    sleep(0.3)
+    pwm.stop()
+
+
+if __name__ == '__main__':
+    servo = int(sys.argv[1])
+    GPIO.setup(servo, GPIO.OUT)
+    set_servo_angle(servo, int(sys.argv[2]))
+    GPIO.cleanup()
