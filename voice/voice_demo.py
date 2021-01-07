b60a6da1b7cff56ba789248947783a0ba3ed929f
cy <micheal.caoy@gmail.com>
2021-01-07 18:04:11 +0800

    -- add voice

--- voice/voice_demo.py
+++ voice/voice_demo.py
@@ -3,10 +3,12 @@
 # author  : CY
 # file    : voice_demo.py
 # modify time:
+from voice.voice_client import VoiceClient
 
 
 def start_voice_demo():
     print('语音功能已开启')
+    VoiceClient()
 
 
 def stop_voice_demo():
