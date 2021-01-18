c911d142a9ceeda77ff40f5ff78d3d48fe16c707
cy <micheal.caoy@gmail.com>
2021-01-18 18:05:01 +0800

    -- 360 func new file.py

--- steering/control_steering.py
+++ steering/control_steering.py
@@ -11,22 +11,18 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 
-def set_servo_angle(foot, angle, max_angle=180):
+def set_servo_angle(foot, angle):
     """
     控制舵机
     servo 脚针
     angle 角度
-    max_angle 舵机的最大角度 默认180
     17 最小值是68 最大值是179"""
     GPIO.setup(foot, GPIO.OUT)
     pwm = GPIO.PWM(foot, 50)
     pwm.start(8)
-    if max_angle == 180: duty_cycle = angle / 18. + 3.
-    elif max_angle == 360: duty_cycle = (1 / 36) * angle + 2.5
-    else: raise Exception(f"最大角度仅支持180与360，你的最大角度{max_angle}")
+    duty_cycle = angle / 18. + 3.
     pwm.ChangeDutyCycle(duty_cycle)
-    if max_angle == 180: sleep(0.3)
-    else: sleep(0.02)
+    sleep(0.3)
     pwm.stop()
     GPIO.cleanup()
 
@@ -34,9 +30,5 @@ def set_servo_angle(foot, angle, max_angle=180):
 if __name__ == '__main__':
     input_foot = int(sys.argv[1])
     input_angle = int(sys.argv[2])
-    if len(sys.argv) == 4:
-        input_max_angle = int(sys.argv[3])
-    else:
-        input_max_angle = 180
     set_servo_angle(input_foot, input_angle)
 
